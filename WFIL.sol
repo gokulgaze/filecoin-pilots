91c5cbd1e49b989822f71a5b5319ed7b9c961b76
Gokul Alex <69299511+gokulgaze@users.noreply.github.com>
2020-11-07 18:41:17 +0530

    Update WFIL.sol

--- WFIL.sol
+++ WFIL.sol
@@ -5,7 +5,7 @@ import "openzeppelin-solidity/contracts/token/ERC20/DetailedERC20.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/MintableToken.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/BurnableToken.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/PausableToken.sol";
-import "../token/WrappedToken.sol";
+import "./WrappedToken.sol";
 
 
 contract WFIL is WrappedToken, DetailedERC20("Wrapped Filecoin", "WFIL", 8) {}
